31560c462116135d3c9347db235a53a5d0b956db
Igor Mazetti <iigormazetti@hotmail.com>
2023-08-21 12:10:53 -0300

    feat: passing the next page number to response at listing users route

--- api/src/modules/users/services/listUsers.service.ts
+++ api/src/modules/users/services/listUsers.service.ts
@@ -7,11 +7,16 @@ type Request = {
 
 export default class ListUsersService {
   public async execute({ since = 1 }: Request) {
+    const regex =
+      /<https:\/\/api\.github\.com\/users\?since=(\d+)&per_page=\d+>; rel="next"/;
     const response = await api.get(`/users?since=${since}&per_page=10`);
+
+    const nextPageNumber = response.headers.link.match(regex);
+    console.log(nextPageNumber[1]);
 
     if (!response)
       throw new CustomError('Request failed to retrieve users', 400);
 
-    return response.data;
+    return { users: response.data, nextPageNumber: Number(nextPageNumber[1]) };
   }
 }
