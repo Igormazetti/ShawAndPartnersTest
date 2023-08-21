d0e5f643eeac10da46ad3317f9faf017106db68b
Igor Mazetti <iigormazetti@hotmail.com>
2023-08-21 16:30:16 -0300

    fix: changed corst origin

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,6 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const port = process.env.PORT || 4000;
-app.use(cors({ origin: ['http://localhost:3000/'] }));
+app.use(cors({ origin: ['*', 'http://localhost:3000/'] }));
 app.listen(port);
 console.log('Server is up!');
