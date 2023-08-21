48049abeb28cc1f8dbcaf07ce474098b5a7eec2d
Igor Mazetti <iigormazetti@hotmail.com>
2023-08-21 16:09:59 -0300

    feat: added cors origin

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,10 @@
 import app from './app';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
-
+app.use(cors({ origin: ['http://localhost:3000/'] }));
 app.listen(port);
 console.log('Server is up!');
