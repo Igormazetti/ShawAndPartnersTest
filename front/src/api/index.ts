313a6682b2255c63a8ab7a3f25efdb0796adce9c
Igor Mazetti <iigormazetti@hotmail.com>
2023-08-22 08:05:39 -0300

    fix: using api url from env variables

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://api-zp06.onrender.com/users',
-  baseURL: 'http://localhost:4000/users',
+  baseURL: process.env.BASE_URL,
+  // baseURL: 'http://localhost:4000/users',
 });
